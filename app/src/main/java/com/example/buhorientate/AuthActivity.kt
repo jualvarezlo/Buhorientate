773a4bb584a37e2ebd3442eac4aef4c097a48dc0
jualvarezlo <jualvarezlo@unal.edu.co>
2022-10-25 21:38:18 -0500

    Inicio registro proveedor

--- app/src/main/java/com/example/buhorientate/AuthActivity.kt
+++ app/src/main/java/com/example/buhorientate/AuthActivity.kt
@@ -3,6 +3,7 @@ package com.example.buhorientate
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
@@ -17,34 +18,13 @@ class AuthActivity : AppCompatActivity() {
         setup()
     }
 
-    private fun setup(){
+    private fun setup() {
         title = "Autenticación"
-        signUpButton.setOnClickListener {
-            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString(),
-                    passwordEditText.text.toString()).addOnCompleteListener{
-                        if (it.isSuccessful){
-                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
-                        }else{
-                            showAlert()
-                        }
-                }
-            }
-        }
-        signInButton.setOnClickListener {
-            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
-                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEditText.text.toString(),
-                    passwordEditText.text.toString()).addOnCompleteListener{
-                    if (it.isSuccessful){
-                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
-                    }else{
-                        showAlert()
-                    }
-                }
-            }
+
+        signUpButton.setOnClickListener{
+            startActivity(Intent(this, ProviderRegister::class.java))
         }
     }
-
     private fun showAlert(){
 
         val builder = AlertDialog.Builder(this)
@@ -55,12 +35,6 @@ class AuthActivity : AppCompatActivity() {
         dialog.show()
     }
 
-    private fun showHome(email: String, provider: ProviderType){
 
-        val homeIntent = Intent(this, HomeActivity::class.java).apply {
-            putExtra("email", email)
-            putExtra("provider", provider.name)
-        }
-        startActivity(homeIntent)
-    }
+
 }
